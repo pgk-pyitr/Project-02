c7f8f41fd71fada5a99be2734cfda4273bcd3318
TonyCode <pgktony615@gmail.com>
2023-06-01 19:58:34 -0500

    added API key

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("searchInput");
+const imageGallery = document.getElementById("imageGallery");
+
+function fetchImages(query) {
+    const apiKey = 'd8UvN6xRkmGof81fM8r0ltx69FmtWMhE'
+   
+
+}
